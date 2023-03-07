9ab1dbe474b8b5aa588d9053381f0d5fdea71733
nihal136 <agarwalnihal136@gmail.com>
2023-03-07 18:54:22 +0530

    React Added

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
 const app = express()
+const h=app.use(express.static("frontend"))//tells to serve all your static files in frontend
 
-app.get("/",function(req,res){
-    res.send("Hello World")
-})
+app.get("/html",function(req,res){
+    res.sendFile(__dirname+"/frontend/index.html");//"dirname" gives location of server file
+});
 
 app.listen(3000,function(){
     console.log("server running on http://localhost:3000")
